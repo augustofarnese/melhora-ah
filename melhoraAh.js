2594852eb0f045c58bc2a08de71bb584e002ffb3
Augusto Campos Farnese <farnese@synergia.dcc.ufmg.br>
2017-01-23 15:58:27 -0200

    Corrige bug que não deixava as horas aparecem quando usuário não tinha tarefa ativa

--- melhoraAh.js
+++ melhoraAh.js
@@ -10,51 +10,51 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';    
+   'use strict';
 
-    var horasNoBancoDeHorasGeral;
-    var saldoDeHorasDoMes;
-    var horasTrabalhadasDoDia;
+   var horasNoBancoDeHorasGeral;
+   var saldoDeHorasDoMes;
+   var horasTrabalhadasDoDia;
 
-		$.ajax({
-			url: 'http://ull/ah/Banco_de_horas.jsp',
-			success: function(data){
-				horasNoBancoDeHorasGeral = parseFloat($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;})[1].data);
-				
-				var d = new Date();
-				var mesAtual = d.getMonth() + 1;
-				var anoAtual = d.getYear() + 1900;
-				mesAtual = ("0" + mesAtual).slice(-2); //Garante que tem 2 dígitos
-				$.ajax({
-					url: 'http://ull/ah/horas_trabalhadas.jsp',
-					data: {
-						dataInicio: '01/' + mesAtual + '/' + anoAtual,
-						dataFim: '',
-						projeto: '-1',
-						txtDescricao: ''
-					},
-					success: function(data){
-						
-				console.log($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;}));
-						saldoDeHorasDoMes = parseFloat($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;})[4].data);
-						$('a[href="Banco_de_horas.jsp"]').after(' <span>(' + converteDecimalParaHoras(horasNoBancoDeHorasGeral + saldoDeHorasDoMes) + ')</span>');
-					}
-				});
-			}
-		});
+        $.ajax({
+            url: 'http://ull/ah/Banco_de_horas.jsp',
+            success: function(data){
+                horasNoBancoDeHorasGeral = parseFloat($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;})[1].data);
+                var d = new Date();
+                var mesAtual = d.getMonth() + 1;
+                var anoAtual = d.getYear() + 1900;
+                mesAtual = ("0" + mesAtual).slice(-2); //Garante que tem 2 dígitos
+                $.ajax({
+                    url: 'http://ull/ah/horas_trabalhadas.jsp',
+                    data: {
+                        dataInicio: '01/' + mesAtual + '/' + anoAtual,
+                        dataFim: '',
+                        projeto: '-1',
+                        txtDescricao: ''
+                    },
+                    success: function(data){
+                       console.log($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;}));
+                       var textosDaPagina = $('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;});                        
+                        saldoDeHorasDoMes = parseFloat(textosDaPagina[textosDaPagina.length - 2].data);
+                        $('a[href="Banco_de_horas.jsp"]').after(' <span>(' + converteDecimalParaHoras(horasNoBancoDeHorasGeral + saldoDeHorasDoMes) + ')</span>');
+                    }
+                });
+            }
+        });
 
 
-    $.ajax({
-        url: 'http://ull/ah/horas_trabalhadas_do_dia.jsp',
-        success: function(data){
-            horasTrabalhadasDoDia = parseFloat($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;})[2].data);
-            $('a[href="horas_trabalhadas_do_dia.jsp"]').after(' <span>(' + converteDecimalParaHoras(horasTrabalhadasDoDia) + ')</span>');
-        }
-    });
+   $.ajax({
+       url: 'http://ull/ah/horas_trabalhadas_do_dia.jsp',
+       success: function(data){
+           var textosDaPagina = $('b:last', data).parent().contents().filter(function(){return (this.nodeType === 3 && this.nodeValue.endsWith("hrs "));});
+           horasTrabalhadasDoDia = parseFloat(textosDaPagina[0].data);
+           $('a[href="horas_trabalhadas_do_dia.jsp"]').after(' <span>(' + converteDecimalParaHoras(horasTrabalhadasDoDia) + ')</span>');
+       }
+   });
 
-    function converteDecimalParaHoras (horaDecimal) {
-        return parseInt(horaDecimal) + ':' + ("0" + parseInt(Math.abs(horaDecimal) % 1 * 60)).slice(-2);;
-    }
+   function converteDecimalParaHoras (horaDecimal) {
+       return parseInt(horaDecimal) + ':' + ("0" + parseInt(Math.abs(horaDecimal) % 1 * 60)).slice(-2);;
+   }
 
 
 
