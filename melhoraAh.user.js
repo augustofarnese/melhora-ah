3dcd9e1b256c0b8c611de5d8885d89871e514dec
Augusto Farnese <farnese@dcc.ufmg.br>
2017-01-30 15:19:23 -0200

    Veriifica as horas novamente após voltar para a aba do AH

--- melhoraAh.user.js
+++ melhoraAh.user.js
@@ -13,11 +13,7 @@
 
 (function () {
     'use strict';
-    /*global localStorage: false, console: false, $: false, prompt: false */
-
-    function converteDecimalParaHoras(horaDecimal) {
-        return parseInt(horaDecimal, 10) + ':' + ("0" + parseInt(Math.abs(horaDecimal) % 1 * 60, 10)).slice(-2);
-    }
+    /*global localStorage: false, console: false, $: false, prompt: false, window: false */
 
     var horasNoBancoDeHorasGeral, saldoDeHorasDoMes, horasTrabalhadasDoDia, dedicacaoDiaria;
 
@@ -26,45 +22,84 @@
     }
     dedicacaoDiaria = localStorage.dedicacaoDiaria;
 
-    $.ajax({
-        url: 'http://ull/ah/Banco_de_horas.jsp',
-        success: function (data) {
-            var d, mesAtual, anoAtual;
-            horasNoBancoDeHorasGeral = parseFloat($('b:last', data).parent().contents().filter(function () { return this.nodeType === 3; })[1].data);
+    function converteDecimalParaHoras(horaDecimal) {
+        return parseInt(horaDecimal, 10) + ':' + ("0" + parseInt(Math.abs(horaDecimal) % 1 * 60, 10)).slice(-2);
+    }
 
-            d = new Date();
-            mesAtual = d.getMonth() + 1;
-            anoAtual = d.getYear() + 1900;
+    function verificaBancoDeHoras() {
+        $.ajax({
+            url: 'http://ull/ah/Banco_de_horas.jsp',
+            success: function (data) {
+                var d, mesAtual, anoAtual;
+                horasNoBancoDeHorasGeral = parseFloat($('b:last', data).parent().contents().filter(function () { return this.nodeType === 3; })[1].data);
 
-            mesAtual = ("0" + mesAtual).slice(-2); //Garante que tem 2 dígitos
-            $.ajax({
-                url: 'http://ull/ah/horas_trabalhadas.jsp',
-                data: {
-                    dataInicio: '01/' + mesAtual + '/' + anoAtual,
-                    dataFim: '',
-                    projeto: '-1',
-                    txtDescricao: ''
-                },
-                success: function (data) {
-                    var textosDaPagina, estiloDoAlerta;
-                    textosDaPagina = $('b:last', data).parent().contents().filter(function () { return this.nodeType === 3; });
+                d = new Date();
+                mesAtual = d.getMonth() + 1;
+                anoAtual = d.getYear() + 1900;
 
-                    saldoDeHorasDoMes = parseFloat(textosDaPagina[textosDaPagina.length - 2].data);
-                    if (Math.abs(saldoDeHorasDoMes) > dedicacaoDiaria * 2) {
-                        estiloDoAlerta = saldoDeHorasDoMes > 0 ? "color:red; font-weight:bold;" : "color:blue;  font-weight:bold;";
+                mesAtual = ("0" + mesAtual).slice(-2); //Garante que tem 2 dígitos
+                $.ajax({
+                    url: 'http://ull/ah/horas_trabalhadas.jsp',
+                    data: {
+                        dataInicio: '01/' + mesAtual + '/' + anoAtual,
+                        dataFim: '',
+                        projeto: '-1',
+                        txtDescricao: ''
+                    },
+                    success: function (data) {
+                        var textosDaPagina, style;
+                        textosDaPagina = $('b:last', data).parent().contents().filter(function () { return this.nodeType === 3; });
+
+                        saldoDeHorasDoMes = parseFloat(textosDaPagina[textosDaPagina.length - 2].data);
+                        if (Math.abs(saldoDeHorasDoMes) > dedicacaoDiaria * 2) {
+                            if (saldoDeHorasDoMes > 0) {
+                                style = {
+                                    color: "red",
+                                    fontWeight: "bold"
+                                };
+                            } else {
+                                style = {
+                                    color: "red",
+                                    fontWeight: "bold"
+                                };
+                            }
+                            $('#saldoBancoDeHoras').css(style);
+                        }
+
+                        $('#saldoBancoDeHoras').text('(' + converteDecimalParaHoras(horasNoBancoDeHorasGeral + saldoDeHorasDoMes) + ')');
                     }
-                    $('a[href="Banco_de_horas.jsp"]').after(' <span style="' + estiloDoAlerta + '">(' + converteDecimalParaHoras(horasNoBancoDeHorasGeral + saldoDeHorasDoMes) + ')</span>');
-                }
-            });
+                });
+            }
+        });
+
+        $.ajax({
+            url: 'http://ull/ah/horas_trabalhadas_do_dia.jsp',
+            success: function (data) {
+                var textosDaPagina = $('b:last', data).parent().contents().filter(function () {return (this.nodeType === 3 && this.nodeValue.endsWith("hrs ")); });
+                horasTrabalhadasDoDia = parseFloat(textosDaPagina[0].data);
+                $('#horasTrabalhadasDoDia').text('(' + converteDecimalParaHoras(horasTrabalhadasDoDia) + ')');
+            }
+        });
+    }
+
+    $(window).on("blur focus", function (e) {
+        var prevType = $(this).data("prevType");
+
+        if (prevType !== e.type) {   //  reduce double fire issues
+            switch (e.type) {
+            case "blur":
+                break;
+            case "focus":
+                verificaBancoDeHoras();
+                break;
+            }
         }
+
+        $(this).data("prevType", e.type);
     });
 
-    $.ajax({
-        url: 'http://ull/ah/horas_trabalhadas_do_dia.jsp',
-        success: function (data) {
-            var textosDaPagina = $('b:last', data).parent().contents().filter(function () {return (this.nodeType === 3 && this.nodeValue.endsWith("hrs ")); });
-            horasTrabalhadasDoDia = parseFloat(textosDaPagina[0].data);
-            $('a[href="horas_trabalhadas_do_dia.jsp"]').after(' <span>(' + converteDecimalParaHoras(horasTrabalhadasDoDia) + ')</span>');
-        }
-    });
+    $('a[href="Banco_de_horas.jsp"]').after(' <span id="saldoBancoDeHoras"></span>');
+    $('a[href="horas_trabalhadas_do_dia.jsp"]').after(' <span id="horasTrabalhadasDoDia"></span>');
+
+    verificaBancoDeHoras();
 })();
