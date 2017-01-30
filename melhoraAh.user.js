eac09fff43f8d123f72fee3b1cda9113b4e65ca6
Augusto Farnese <farnese@dcc.ufmg.br>
2017-01-30 14:54:30 -0200

    Alterações de identação

--- melhoraAh.user.js
+++ melhoraAh.user.js
@@ -2,7 +2,7 @@
 // @name         MelhoraAH
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  Adiciona informações importantes ao AH!
+// @description  Para facilitar a gestão do banco de horas, o MelhoraAH adiciona ao menu do AH informações sobre o banco de horas e as horas trabalhadas no dia.
 // @author       Augusto Farnese
 // @match        http://ull/ah/*
 // @grant        none
@@ -11,65 +11,60 @@
 // @downloadURL  https://raw.githubusercontent.com/augustofarnese/melhora-ah/master/melhoraAh.user.js
 // ==/UserScript==
 
-(function() {
-   'use strict';
+(function () {
+    'use strict';
+    /*global localStorage: false, console: false, $: false, prompt: false */
 
-   var horasNoBancoDeHorasGeral;
-   var saldoDeHorasDoMes;
-   var horasTrabalhadasDoDia;
-   var dedicacaoDiaria;
-    
-   if(!localStorage.dedicacaoDiaria){
-       localStorage.dedicacaoDiaria = prompt("Quantas horas você trabalha por dia?");
-   }
+    function converteDecimalParaHoras(horaDecimal) {
+        return parseInt(horaDecimal, 10) + ':' + ("0" + parseInt(Math.abs(horaDecimal) % 1 * 60, 10)).slice(-2);
+    }
+
+    var horasNoBancoDeHorasGeral, saldoDeHorasDoMes, horasTrabalhadasDoDia, dedicacaoDiaria;
+
+    if (!localStorage.dedicacaoDiaria) {
+        localStorage.dedicacaoDiaria = prompt("Quantas horas você trabalha por dia?");
+    }
     dedicacaoDiaria = localStorage.dedicacaoDiaria;
-  
 
-        $.ajax({
-            url: 'http://ull/ah/Banco_de_horas.jsp',
-            success: function(data){
-                horasNoBancoDeHorasGeral = parseFloat($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;})[1].data);
-                var d = new Date();
-                var mesAtual = d.getMonth() + 1;
-                var anoAtual = d.getYear() + 1900;
-                mesAtual = ("0" + mesAtual).slice(-2); //Garante que tem 2 dígitos
-                $.ajax({
-                    url: 'http://ull/ah/horas_trabalhadas.jsp',
-                    data: {
-                        dataInicio: '01/' + mesAtual + '/' + anoAtual,
-                        dataFim: '',
-                        projeto: '-1',
-                        txtDescricao: ''
-                    },
-                    success: function(data){
-                       console.log($('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;}));
-                       var textosDaPagina = $('b:last', data).parent().contents().filter(function(){return this.nodeType === 3;});                        
-                       var estiloDoAlerta;
-                       saldoDeHorasDoMes = parseFloat(textosDaPagina[textosDaPagina.length - 2].data);
-                       console.log(Math.abs(saldoDeHorasDoMes) > dedicacaoDiaria * 2);
-                       if(Math.abs(saldoDeHorasDoMes) > dedicacaoDiaria * 2){
-                          estiloDoAlerta = saldoDeHorasDoMes > 0 ? "color:red; font-weight:bold;" : "color:blue;  font-weight:bold;";
-                       }
-                       $('a[href="Banco_de_horas.jsp"]').after(' <span style="' + estiloDoAlerta + '">(' + converteDecimalParaHoras(horasNoBancoDeHorasGeral + saldoDeHorasDoMes) + ')</span>');
+    $.ajax({
+        url: 'http://ull/ah/Banco_de_horas.jsp',
+        success: function (data) {
+            var d, mesAtual, anoAtual;
+            horasNoBancoDeHorasGeral = parseFloat($('b:last', data).parent().contents().filter(function () { return this.nodeType === 3; })[1].data);
+
+            d = new Date();
+            mesAtual = d.getMonth() + 1;
+            anoAtual = d.getYear() + 1900;
+
+            mesAtual = ("0" + mesAtual).slice(-2); //Garante que tem 2 dígitos
+            $.ajax({
+                url: 'http://ull/ah/horas_trabalhadas.jsp',
+                data: {
+                    dataInicio: '01/' + mesAtual + '/' + anoAtual,
+                    dataFim: '',
+                    projeto: '-1',
+                    txtDescricao: ''
+                },
+                success: function (data) {
+                    var textosDaPagina, estiloDoAlerta;
+                    textosDaPagina = $('b:last', data).parent().contents().filter(function () { return this.nodeType === 3; });
+
+                    saldoDeHorasDoMes = parseFloat(textosDaPagina[textosDaPagina.length - 2].data);
+                    if (Math.abs(saldoDeHorasDoMes) > dedicacaoDiaria * 2) {
+                        estiloDoAlerta = saldoDeHorasDoMes > 0 ? "color:red; font-weight:bold;" : "color:blue;  font-weight:bold;";
                     }
-                });
-            }
-        });
+                    $('a[href="Banco_de_horas.jsp"]').after(' <span style="' + estiloDoAlerta + '">(' + converteDecimalParaHoras(horasNoBancoDeHorasGeral + saldoDeHorasDoMes) + ')</span>');
+                }
+            });
+        }
+    });
 
-
-   $.ajax({
-       url: 'http://ull/ah/horas_trabalhadas_do_dia.jsp',
-       success: function(data){
-           var textosDaPagina = $('b:last', data).parent().contents().filter(function(){return (this.nodeType === 3 && this.nodeValue.endsWith("hrs "));});
-           horasTrabalhadasDoDia = parseFloat(textosDaPagina[0].data);
-           $('a[href="horas_trabalhadas_do_dia.jsp"]').after(' <span>(' + converteDecimalParaHoras(horasTrabalhadasDoDia) + ')</span>');
-       }
-   });
-
-   function converteDecimalParaHoras (horaDecimal) {
-       return parseInt(horaDecimal) + ':' + ("0" + parseInt(Math.abs(horaDecimal) % 1 * 60)).slice(-2);
-   }
-
-
-
+    $.ajax({
+        url: 'http://ull/ah/horas_trabalhadas_do_dia.jsp',
+        success: function (data) {
+            var textosDaPagina = $('b:last', data).parent().contents().filter(function () {return (this.nodeType === 3 && this.nodeValue.endsWith("hrs ")); });
+            horasTrabalhadasDoDia = parseFloat(textosDaPagina[0].data);
+            $('a[href="horas_trabalhadas_do_dia.jsp"]').after(' <span>(' + converteDecimalParaHoras(horasTrabalhadasDoDia) + ')</span>');
+        }
+    });
 })();
