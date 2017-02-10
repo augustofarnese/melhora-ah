f033dcfa1ffb8da99fa5cc56f26211f4cf3ce600
Augusto Farnese <farnese@dcc.ufmg.br>
2017-02-10 14:21:39 -0200

    Remove mensagens no console

--- melhoraAh.user.js
+++ melhoraAh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MelhoraAH
 // @namespace    http://tampermonkey.net/
-// @version      0.3.3
+// @version      0.3.4
 // @description  Para facilitar a gestão do banco de horas, o MelhoraAH adiciona ao menu do AH informações sobre o banco de horas e as horas trabalhadas no dia.
 // @author       Augusto Farnese
 // @match        http://ull/ah/*
@@ -50,9 +50,7 @@
                         var textosDaPagina, style;
                         textosDaPagina = $('b:last', data).parent().contents().filter(function () { return this.nodeType === 3; });
                         saldoDeHorasDoMes = parseFloat(textosDaPagina[textosDaPagina.length - 2].data);
-                        console.log(saldoDeHorasDoMes);
                         saldoDeHorasDoMes += horasNoBancoDeHorasGeral;
-                        console.log(saldoDeHorasDoMes);
                         if (Math.abs(saldoDeHorasDoMes) > dedicacaoDiaria * 2) {
                             if (saldoDeHorasDoMes > 0) {
                                 style = {
